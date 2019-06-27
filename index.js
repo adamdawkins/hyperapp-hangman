0a2be08e0ae5520e7b2d26753fa74593674b77a4
Adam Dawkins <adam@dragondrop.uk>
2019-06-27 18:59:48 +0100

    Making things a bit more 'hangmany'

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import { h, app } from 'hyperapp'
-import { div, h1, h2 } from '@hyperapp/html'
+import { div, h1, h2, ul, li } from '@hyperapp/html'
 
 app({
 	init: () => ({
 		word: 'application',
-		guesses: [],
+		guesses: ['a', 'b', 'c', 'd'],
 	}), // we don't have a state here
 	view: (state) => (
 		div({},
 			[ h1({}, state.word)
 			, h2({}, 'Your Guesses:')
+				, ul({ class: 'guesses' },
+					state.guesses.map(guess => 
+						li({ class: 'guess' }, guess)
+					)
+				)
 			]
 		)
 	),
   node: document.getElementById('app')
 })
-
