48244f9873d14dfe62c5aa9e62a6aeab6d74a173
Adam Dawkins <adam@dragondrop.uk>
2019-06-27 19:09:40 +0100

    Hide the word

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import { h, app } from 'hyperapp'
-import { div, h1, h2, ul, li } from '@hyperapp/html'
+import { div, h1, h2, ul, li, span } from '@hyperapp/html'
+
+const mdash = "\u2014";
 
 app({
 	init: () => ({
-		word: 'application',
+		word: 'application'.split(''),
 		guesses: ['a', 'b', 'c', 'd'],
-	}), // we don't have a state here
+	}),
 	view: (state) => (
 		div({},
-			[ h1({}, state.word)
+			[ h1({}, state.word.map(() => span({class: 'letter'}, mdash)))
 			, h2({}, 'Your Guesses:')
 				, ul({ class: 'guesses' },
 					state.guesses.map(guess => 
